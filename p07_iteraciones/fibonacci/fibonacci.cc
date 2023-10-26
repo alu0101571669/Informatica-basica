06b6065e0c9fc2fade29719a5878db7578e41bbf
usuario <alu0101571669@ull.edu.es>
2023-10-26 17:43:06 +0000

    26/10/2023

--- p07_iteraciones/fibonacci/fibonacci.cc
+++ p07_iteraciones/fibonacci/fibonacci.cc
@@ -14,17 +14,33 @@ using namespace std;
   * @brief Programa que imprime los N números de la serie de Fibonacci
 */
 
-int main() {
-  int numero, first, second, suma;
-  first = 0; // Primer número de la sucesión de fibonacci
-  second = 1; // Segundo número de la sucesión de fibonacci
-  cin >> numero;
-  for(int it = 0; it < numero; it++) {
+void PrintProgramPurpose() {
+  cout << "Programa que imprima los n términos de la serie de Fibonacci" << endl;
+  cout << "Número: ";
+}
+
+/**
+ * @brief Realiza la suma de los términos de la serie de Fibonacci
+ * @param numero Número para saber cuando hay que parar de hacer la sucesión
+*/
+
+void Fibonacci(int numero) {
+  int suma; // Almacena la suma de los números de la serie
+  int first = 0; // Primer número de la serie
+  int second = 1; // Segundo número de la serie
+  for (int it = 0; it < numero; it++) {
     cout << first << " ";
     suma = first + second;
     second = first;
     first = suma;
   }
+}
+
+int main() {
+  PrintProgramPurpose();
+  int numero;
+  cin >> numero;
+  Fibonacci(numero);
   cout << endl;
   return 0;
 }
