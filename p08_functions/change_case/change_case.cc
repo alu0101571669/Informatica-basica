3b5ca5187f42d61ce36a8e952917bfdba445f861
usuario <alu0101571669@ull.edu.es>
2023-11-02 18:01:32 +0000

    02/11/2023

--- p08_functions/change_case/change_case.cc
+++ p08_functions/change_case/change_case.cc
@@ -3,6 +3,18 @@
 
 using namespace std;
 
+/*
+  * Universidad de La Laguna
+  * Escuela Superior de Ingeniería y Tecnología
+  * Grado en Ingeniería Informática
+  * Informática Básica 2023-2024
+  *
+  * @file change_case.cc
+  * @author Franco Alla alu0101571669@ull.edu.es
+  * @date 02/11/2023
+  * @brief Programa que convierte las letras mayúsculas a minúsculas y viceversa
+*/
+
 /**
  * @brief Muestra el funcionamiento del programa
 */
