553a58e727e9be616cdb04164fdaf4bb462ffef7
usuario <alu0101571669@ull.edu.es>
2023-11-08 16:54:34 +0000

    08/11/2023

--- p09_funcions2/scalar_product/scalar_product.h
+++ p09_funcions2/scalar_product/scalar_product.h
@@ -3,6 +3,6 @@
 
 using namespace std;
 
-double ScalarProduct(vector<double>&, vector<double>&);
-bool IsEqual(int, int);
-void PrintVector(vector<double>&);
+double ScalarProduct(vector<double>&, vector<double>&); // Calcular el producto escalar de los vectores
+bool IsEqual(int, int); // Comprobar que el tamaño de los vectores es el mismo
+void PrintVector(vector<double>&); // Imprimir los vectores
