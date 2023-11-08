b43b5e0670618d5e2b3ec4fdb18bf1d002874267
usuario <alu0101571669@ull.edu.es>
2023-11-08 16:51:22 +0000

    08/11/2023

---
+++ p09_funcions2/scalar_product/scalar_product.h
@@ -0,0 +1,8 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+double ScalarProduct(vector<double>&, vector<double>&);
+bool IsEqual(int, int);
+void PrintVector(vector<double>&);
