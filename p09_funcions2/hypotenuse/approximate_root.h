1c86b88fa1d1d8e84ecdce0bfcd4380631b3dc93
usuario <alu0101571669@ull.edu.es>
2023-11-12 01:30:19 +0000

    10/11/2023

---
+++ p09_funcions2/hypotenuse/approximate_root.h
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <cmath>
+#define EPSILON 1e-4
+
+using namespace std;
+
+/*
+  * Universidad de La Laguna
+  * Escuela Superior de Ingeniería y Tecnología
+  * Grado en Ingeniería Informática
+  * Informática Básica 2023-2024
+  *
+  * @file approximate_root.h
+  * @author Franco Alla alu0101571669@ull.edu.es
+  * @date 09/11/2023
+  * @brief Declaración de las funciones del programa
+*/
+
+double Approximate(int); // Calcular la raíz cuadrada
